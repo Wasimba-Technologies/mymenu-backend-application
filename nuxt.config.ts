92be9548fd8c268c6463273162ed05d403bf7e85
Valence Kajuna <valencekajuna@yahoo.com>
2022-07-15 11:36:18 +0300

    transpile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   build: {
+    transpile: ['@heroicons/vue'],
     postcss: {
       postcssOptions: {
         plugins: {
