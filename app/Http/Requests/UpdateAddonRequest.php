9776e579105ec3f8b5cb5a6f2562a077b9c20e84
njunwa1 <njunwavictor@gmail.com>
2023-09-11 19:51:48 +0300

    adds addon DTO

--- app/Http/Requests/UpdateAddonRequest.php
+++ app/Http/Requests/UpdateAddonRequest.php
@@ -11,7 +11,7 @@ class UpdateAddonRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateAddonRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|max:255',
+            'is_incrementing' => ['required', 'boolean'],
+            'price' => ['required', 'integer'],
+            'type' => ['required', 'string'],
         ];
     }
 }
--- app/Http/Resources/AddonResource.php
+++ app/Http/Resources/AddonResource.php
@@ -14,6 +14,12 @@ class AddonResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'is_incrementing' => $this->is_incrementing,
+            'price' => $this->price,
+            'type' => $this->type,
+        ];
     }
 }
