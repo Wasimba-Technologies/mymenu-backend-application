2b11c5523519abecdfe782bb0c24c80d68988023
njunwa1 <njunwavictor@gmail.com>
2023-09-11 20:25:24 +0300

    adds one to many relation btn Variation and Values

--- app/Models/Variation.php
+++ app/Models/Variation.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Variation extends Model
 {
     use HasFactory, BelongsToTenant;
 
         protected $guarded = ['id'];
+
+    public function variationValues(): HasMany
+    {
+        return $this->hasMany(VariationValue::class);
+    }
 }
--- app/Models/VariationValue.php
+++ app/Models/VariationValue.php
@@ -5,10 +5,16 @@ namespace App\Models;
 use App\Traits\BelongsToTenant;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class VariationValue extends Model
 {
     use HasFactory, BelongsToTenant;
 
         protected $guarded = ['id'];
+
+    public function variation(): BelongsTo
+    {
+        return $this->belongsTo(Variation::class);
+    }
 }
