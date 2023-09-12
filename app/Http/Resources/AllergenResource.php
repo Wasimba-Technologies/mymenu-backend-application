d777d09c068281d631f85cffcbeb67e540c8b303
njunwa1 <njunwavictor@gmail.com>
2023-09-12 12:34:37 +0300

    Adds the Allergen DTO/DAO

--- app/Http/Resources/AllergenResource.php
+++ app/Http/Resources/AllergenResource.php
@@ -14,6 +14,9 @@ class AllergenResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name
+        ];
     }
 }
--- database/migrations/2023_09_12_093108_create_allergens_table.php
+++ database/migrations/2023_09_12_093108_create_allergens_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('allergens', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->string('tenant_id', 32)->nullable();
             $table->timestamps();
         });
