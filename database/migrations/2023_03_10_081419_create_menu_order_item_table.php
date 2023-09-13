3a85131ea45b24090e818c6e8bac4da0f5b1d6a9
njunwa1 <njunwavictor@gmail.com>
2023-09-13 20:02:23 +0300

    removes a migration file

--- database/migrations/2023_03_10_081419_create_menu_order_item_table.php
+++ database/migrations/2023_03_10_081419_create_menu_order_item_table.php
@@ -15,6 +15,13 @@ return new class extends Migration
             $table->id();
             $table->foreignId('order_id')->constrained('id')->on('orders');
             $table->foreignId('menu_item_id')->constrained('id')->on('menu_items');
+            $table->json('ingredients')->nullable();
+            $table->json('addons')->nullable();
+            $table->json('variation_values')->nullable();
+            $table->text('instructions')->nullable();
+            //adds the price of the menu item at the time of order
+            //because price cannot be cheaply traced back based on the added ingredients,addons,variations
+            $table->unsignedDecimal('price', 12, 2);
             $table->unsignedInteger('qty');
             //$table->string('tenant_id', 32)->nullable();
             //$table->timestamps();
